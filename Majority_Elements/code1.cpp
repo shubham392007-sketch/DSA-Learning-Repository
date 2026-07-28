102ed74c4a3bc40e6e962fcc3c729070d05ae0eb
shubham392007-sketch <shubham392007@gmail.com>
2026-07-28 07:16:47 +0530

    Added code file in Majority Elements

--- Majority_Elements/code1.cpp
+++ Majority_Elements/code1.cpp
@@ -1,8 +1,47 @@
-//Pair sum
+// Pair sum - brute force approach
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
+    int n;
+    cout << "Enter number of elements you want to enter in array: ";
+    cin >> n;
+    int array[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the value at index " << i << " : ";
+        cin >> array[i];
+    }
+    cout << "Your array is is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+    vector<int> vec_1;
+    int target;
+    cout << "Enter your target value which must be the sum of the pairs present in array: ";
+    cin >> target;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (array[i] + array[j] == target)
+            {
+                vec_1.push_back(array[i]);
+                vec_1.push_back(array[j]);
+            }
+        }
+    }
+
+    cout << "The pair whose sum is equal to target value " << target << " is: ";
+    for (int val : vec_1)
+    {
+        cout << val << " ";
+    }
     return 0;
 }
